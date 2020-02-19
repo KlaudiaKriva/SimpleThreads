a7b143b4ab4b124034d75441b969868e953d6de2
Klaudia Kriva <klaudia.kriva@akademiasovy.sk>
2020-02-19 09:55:42 +0100

    threads initial

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main started");
+        Vlakno vlakno = new Vlakno("Mima");
+        vlakno.startXXX();
+        Vlakno vlakno1 = new Vlakno("Klaudi");
+        vlakno1.startXXX();
+        Vlakno vlakno2 = new Vlakno("Tomas");
+        vlakno2.startXXX();
+        System.out.println("Main finished");
+    }
+}
